222fcf6eb2b8e5baff1e5714e5a520bd5084dc3b
Emmrys-Jay <jonathanemma121@gmail.com>
2022-01-10 06:06:55 +0100

    Fixed the UI of the rock-paper-scissors game.

--- game.js
+++ game.js
@@ -1,68 +1,70 @@
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const display = document.querySelector('.display');
+
+let compScore = 0, playerScore = 0;
+
+
+rock.addEventListener('click', () => {
+    playRound('rock');
+});
+paper.addEventListener('click', () => {
+    playRound('paper');
+});
+scissors.addEventListener('click', () => {
+    playRound('scissors');
+});
+
 function computerPlay(){
     let selectedVal = Math.floor(Math.random()*3);
-    if (selectedVal === 0) return "Rock";
-    else if (selectedVal === 1) return "Paper";
-    else if (selectedVal === 2) return "Scissors";
+    if (selectedVal === 0) return "rock";
+    else if (selectedVal === 1) return "paper";
+    else if (selectedVal === 2) return "scissors";
 }
 
-function playGame(){
-    playerSelection = prompt("What's your game? ");
-    insensPlayerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection){
     computerSelection = computerPlay();
-    insensCompSelection = computerSelection.toLowerCase();
-
-    if (insensPlayerSelection === "rock" && insensCompSelection == "paper"){
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        return (`You lose! ${computerSelection} beats ${playerSelection}`);
-    } else if (insensPlayerSelection === "rock" && insensCompSelection === "scissors"){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return (`You win! ${playerSelection} beats ${computerSelection}`);
-    } else if (insensPlayerSelection === "paper" && insensCompSelection === "scissors"){
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        return (`You lose! ${computerSelection} beats ${playerSelection}`);
-    } else if (insensPlayerSelection === "paper" && insensCompSelection === "rock"){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return (`You win! ${playerSelection} beats ${computerSelection}`);
-    } else if (insensPlayerSelection === "scissors" && insensCompSelection === "rock"){
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        return (`You lose! ${computerSelection} beats ${playerSelection}`);
-    } else if (insensPlayerSelection === "scissors" && insensCompSelection === "paper"){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return (`You win! ${playerSelection} beats ${computerSelection}`);
-    } else if (insensCompSelection === insensPlayerSelection){
-        console.log(`Its a draw! ${playerSelection} draws ${computerSelection}`);
-        return 1;
-    } else {
-        console.log(`Invalid Input`);
-        return 0;
-    }
-}
-
-function gameReady(){
-    let compScore = 0, playerScore = 0;
-
-    for (let i = 0; i < 5; i++){
-        let gameResult = playGame();
-        console.log(gameResult[4]);
-        if (gameResult[4] === "l"){
-            compScore += 1;
-        } else if (gameResult[4] === "w"){
-            playerScore += 1;
-        } else if (gameResult === 1){
-            continue;
-        } else {
-            i -= 1;
-            continue;
-        }
-    }
-
-    if (playerScore > compScore){
-        console.log(`Your score: ${playerScore}  Computer score: ${compScore} \n You win!`);
-    } else if (compScore > playerScore){
-        console.log(`Your score: ${playerScore}  Computer score: ${compScore} \n You lose!`);
+    let text = "";
+    if (playerSelection === "rock" && computerSelection == "paper"){
+        text = `<p>You lose! ${computerSelection} beats ${playerSelection}. `;
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        text = `<p>You win! ${playerSelection} beats ${computerSelection}. `;
+    } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        text = `<p>You lose! ${computerSelection} beats ${playerSelection}. `;
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        text = `<p>You win! ${playerSelection} beats ${computerSelection}. `;
+    } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        text = `<p>You lose! ${computerSelection} beats ${playerSelection}. `;
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        text = `<p>You win! ${playerSelection} beats ${computerSelection}. `;
     } else{
-        console.log(`Your score: ${playerScore}  Computer score: ${compScore} \n It's a draw!`);
+        text = `<p>Its a draw! ${playerSelection} draws ${computerSelection}. `;
     }
+
+    calcScores(text);
 }
 
+function calcScores(text){
+    if (text[7] === "l"){
+        compScore += 1;
+        text += `<br> Your score: ${playerScore} Computer score: ${compScore}`;
+    } else if (text[7] === "w"){
+        playerScore += 1;
+        text += `<br> Your score: ${playerScore} Computer score: ${compScore}`;
+    } else {
+        text += `<br> Your score: ${playerScore} Computer score: ${compScore}`;
+    }
+    //console.log(text);
+    if (compScore === 5){
+        text += `<br>You lose the game. Play again by clicking on your next choice`;
+        compScore = playerScore = 0;
+    } else if (playerScore === 5){
+        text += `<br>You win the game. Play again by clicking on your next choice`;
+        compScore = playerScore = 0;
+    }
+    text += "</p>";
+    display.innerHTML = text;
+
+}
 
